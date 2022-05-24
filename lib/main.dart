ef31c43aaca96e43a9d3a239d1793b85fe945bf2
arshiaor <orangashi@yahoo.com>
2022-05-24 16:10:37 +0430

    noob codes

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+
+  List <Icon> scoreKeeper=[];
+
+  List <String> questions = [
+    'If you travel faster than the speed of light, you’ll age less',
+    'Pi is an irrational number',
+    'Time goes slower at the top of the building than at the bottom',
+    'The gyroscopic effect keeps a bike balanced',
+  ];
+
+  List <bool> answers =[
+    true,
+    false,
+    false,
+    true
+  ];
+
+  int questionNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions[questionNumber],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+
+                bool correctAnswer = answers[questionNumber];
+                if(correctAnswer == true){
+                    print('user got it right');
+                }else{
+                  print('user got it wrong');
+                }
+
+                setState(() {
+                  scoreKeeper.add(Icon(Icons.check,color: Colors.green,));
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+
+                bool correctAnswer = answers[questionNumber];
+                if(correctAnswer == false){
+                  print('user got it right');
+                }else{
+                  print('user got it wrong');
+                }
+
+                setState(() {
+                  questionNumber++;
+                });
+                },
+            ),
+          ),
+        ),
+
+        Row(
+          children: scoreKeeper,
+        ),
+      ],
+    );
+  }
+}
+
+/*
+'If you travel faster than the speed of light, you’ll age less' true
+'Pi is an irrational number' false
+'Time goes slower at the top of the building than at the bottom' false
+'The gyroscopic effect keeps a bike balanced' true
+*/
